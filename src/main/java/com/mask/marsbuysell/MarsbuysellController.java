c2d3ba29bc52ad4bd3be3060c9412c60d8484f66
sheepover96 <sheep.over@gmail.com>
2023-10-08 11:44:18 +0900

    add api server

--- src/main/java/com/mask/marsbuysell/MarsbuysellController.java
+++ src/main/java/com/mask/marsbuysell/MarsbuysellController.java
@@ -1,21 +1,26 @@
 package com.mask.marsbuysell;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 
-@Controller
+@RestController
 public class MarsbuysellController {
 
-	@RequestMapping(value="/demo")
-	private String hello(){
-		return "/index.html";
-	}
+    @GetMapping("/api/data")
+    public ResponseEntity<Object> getData() {
+        // レスポンスのデータを作成
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("key1", "value1");
+        responseData.put("key2", "value2");
 
-    @GetMapping("/hello")
-    public String hello(Model model) {
-        model.addAttribute("message", "Hello, World!");
-        return "helloworld"; // この文字列はテンプレートファイルの名前です
+        return new ResponseEntity<>(responseData, HttpStatus.OK);
     }
 }
